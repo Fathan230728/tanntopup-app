6e094c1dd24254d37180cec5eb7143b984f766c6
Fathan230728 <mfathanr87@gmail.com>
2025-05-21 15:11:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,12 @@ st.write(
     "Top up murah dan terpercaya hanya di TanTopUp."
 )
 st.image("IMG_20250520_165409.jpg", width=200) 
+
+st. title(“topup termurah") 
+st. header("aplikasi untuk topup semua game") 
+angka = st. number_input("tuliskan sebuah angka:", value=0, step=1) 
+
+if (angka % 2) == 0:
+    st. write(f"{angka} gacor kang") 
+else:
+    st. write(f"{angka} habisin duitnya disni😡") 
