8c7597b5def0112569c2cc86d5ed35957c140a43
Fathan230728 <mfathanr87@gmail.com>
2025-05-21 15:01:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("tanntopup")
 st.write(
     "Top up murah dan terpercaya hanya di TanTopUp."
 )
-st.image("IMG-20250520-165409.jpg",width=200) 
+st.image("IMG_20250520_165409.jpg",width=300) 
